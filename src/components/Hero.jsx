a549ac630cbd263b39f93a8b20fe4ab6c3e8e0cd
Maricely Villalobos <maricely.villalobos21@gmail.com>
2025-03-22 21:59:59 -0500

    adding a carrusel to show my Credly badges

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,6 +1,16 @@
 import React, { useRef, useState } from "react";
+import Slider from "react-slick";
 import my2 from "../assets/images/hireMe.png";
 import Type from "./Type";
+
+import Badge1 from "../assets/images/badges/Badge1.png";
+import Badge2 from "../assets/images/badges/Badge2.png";
+import Badge3 from "../assets/images/badges/Badge3.png";
+import Badge4 from "../assets/images/badges/Badge4.png";
+import Badge5 from "../assets/images/badges/Badge5.png";
+import Badge6 from "../assets/images/badges/Badge6.png";
+import Badge7 from "../assets/images/badges/Badge7.png";
+import Badge8 from "../assets/images/badges/Badge8.png";
 
 const socialMediaLinks = [
   "https://www.linkedin.com/in/maricelyvq/",
@@ -9,14 +19,22 @@ const socialMediaLinks = [
   "https://twitter.com/maricelyvq_",
 ];
 
-
+const badgeImages = [
+  Badge1,
+  Badge2,
+  Badge3,
+  Badge4,
+  Badge5,
+  Badge6,
+  Badge7,
+  Badge8,
+];
 
 const Hero = () => {
   const contactRef = useRef(null);
   const [animateIcons, setAnimateIcons] = useState(false);
 
   const animateSocialMedia = () => {
-    // contactRef.current.scrollIntoView({ behavior: 'smooth' });
     setAnimateIcons(true);
     setTimeout(() => setAnimateIcons(false), 1000);
   };
@@ -27,6 +45,18 @@ const Hero = () => {
     "logo-instagram",
     "logo-twitter",
   ];
+
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 4000,
+    arrows: false,
+  };
+
   return (
     <section
       id="home"
@@ -45,10 +75,16 @@ const Hero = () => {
             My Name is <span>Maricely Villalobos</span>
           </h1>
           <h4 className="md:text-2xl text-lg md:leading-normal leading-5 mt-4 font-bold text-gray-300">
-                <Type/>
+            <Type />
           </h4>
-          <button className="btn-primary mt-8" onClick={animateSocialMedia}>Connect with Me</button>
-          <div className={`mt-8 text-3xl flex items-center md:justify-start justify-center gap-10 ${animateIcons ? 'animate-icons' : ''}`}>
+          <button className="btn-primary mt-8" onClick={animateSocialMedia}>
+            Connect with Me
+          </button>
+          <div
+            className={`mt-8 text-3xl flex items-center md:justify-start justify-center gap-10 ${
+              animateIcons ? "animate-icons" : ""
+            }`}
+          >
             {social_media?.map((icon, index) => (
               <a
                 key={icon}
@@ -60,6 +96,16 @@ const Hero = () => {
                 <ion-icon name={icon}></ion-icon>
               </a>
             ))}
+          </div>
+          {/* <a href="https://www.credly.com/users/maricelyvq" target="_blank" rel="noopener noreferrer"></a> */}
+          <div className="w-full justify-center mt-10 max-w-xs sm:max-w-sm md:max-w-md lg:max-w-lg xl:max-w-xl"> 
+            <Slider {...settings}>
+              {badgeImages.map((image, index) => (
+                <div key={index} className="p-2 flex justify-center items-center">
+                  <img src={image} alt={`Badge ${index + 1}`} className="w-18 h-18 object-contain" />
+                </div>
+              ))}
+            </Slider>
           </div>
         </div>
       </div>
